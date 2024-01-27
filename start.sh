1a629b10d77a15151f397ddd1d89574ac003db8d
123456mbhjkm <96689614+123456mbhjkm@users.noreply.github.com>
2024-01-27 16:02:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,12 +5,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/VJ-FILTER-BOT.git /VJ-FILTER-BOT 
+  git clone https://github.com/123456mbhjkm/Telegram-File-Stream-Bot.git /Telegram-File-Stream-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /VJ-FILTER-BOT 
+  git clone $UPSTREAM_REPO /Telegram-File-Stream-Bot
 fi
-cd /VJ-FILTER-BOT 
+cd /Telegram-File-Stream-Bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
